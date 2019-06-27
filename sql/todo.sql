ec8ceb8a1272f83603ecab4f4253389aa35ddb52
小林 <@TSD-KOBAYASHI>
2019-06-27 13:51:56 +0900

    修正

--- sql/todo.sql
+++ sql/todo.sql
@@ -2,19 +2,20 @@
 create table todo(
  number int,
  title varchar(50),
+ detail varchar(100),
  priority varchar(3),
  deadline date 
 );
 
-insert into todo(number, title, priority, deadline)
-values(1,'テストテスト','★★★','2015-06-20');
+insert into todo(number, title, detail, priority, deadline)
+values(1,'テストテスト','SQLの確認テストの採点と報告書を作成する。','★★★','2015-06-20');
 
-insert into todo(number, title, priority, deadline)
-values(2,'テストテスト','★','2015-06-20');
+insert into todo(number, title, detail, priority, deadline)
+values(2,'テストテスト','SQLの確認テストの採点と報告書を作成する。','★','2015-06-20');
 
-insert into todo(number, title, priority, deadline)
-values(3,'テストテスト','★★★','2015-06-20');
+insert into todo(number, title, detail, priority, deadline)
+values(3,'テストテスト','SQLの確認テストの採点と報告書を作成する。','★★★','2015-06-20');
 
-insert into todo(number, title, priority, deadline)
-values(4,'テストテスト','★★★',null);
+insert into todo(number, title, detail, priority, deadline)
+values(4,'テストテスト','SQLの確認テストの採点と報告書を作成する。', '★★★',null);
 
